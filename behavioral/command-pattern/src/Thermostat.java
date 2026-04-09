640449865090e904a98403d4948156b3968ab262
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2026-04-09 12:33:52 +0000

    First Commit

---
+++ behavioral/command-pattern/src/Thermostat.java
@@ -0,0 +1,13 @@
+
+public class Thermostat {
+    private double temperature = 22.0;
+
+    public void setTemperature(double temp) {
+        System.out.println("Thermostat: temperature set from " + temperature + " to " + temp);
+        temperature = temp;
+    }
+
+    public void increase(double delta) { setTemperature(temperature + delta); }
+    public void decrease(double delta) { setTemperature(temperature - delta); }
+    public double getTemperature() { return temperature; }
+}
