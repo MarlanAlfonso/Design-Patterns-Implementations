d56db790d85c2f7e02049d5651a1cca55ea16b42
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2026-04-09 14:04:29 +0000

    Commit

---
+++ structural/facade-pattern/src/Cart.java
@@ -0,0 +1,10 @@
+public class Cart implements HotelService {
+    @Override
+    public String serviceName() {
+        return "Cart Service";
+    }
+
+    public void requestCart(int numberOfCarts) {
+        System.out.printf("[Cart] Requesting %d luggage cart(s)... delivered.%n", numberOfCarts);
+    }
+}
---
+++ structural/facade-pattern/src/HouseKeeping.java
@@ -0,0 +1,10 @@
+public class HouseKeeping implements HotelService {
+    @Override
+    public String serviceName() {
+        return "HouseKeeping Service";
+    }
+
+    public void cleanRoom(int roomNumber) {
+        System.out.printf("[HouseKeeping] Cleaning room %d... completed.%n", roomNumber);
+    }
+}
