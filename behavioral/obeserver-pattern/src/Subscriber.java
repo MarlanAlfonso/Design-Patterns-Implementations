9fdda71338bb53e4a0c799fce28315ce372e7b22
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2026-04-09 14:50:40 +0000

    Commit

---
+++ behavioral/obeserver-pattern/src/Subscriber.java
@@ -0,0 +1,12 @@
+
+import java.util.Set;
+
+public interface Subscriber {
+    void update(News news);
+
+    boolean isInterested(News news);
+
+    void setPreferences(Set<String> categories);
+
+    String getName();
+}
